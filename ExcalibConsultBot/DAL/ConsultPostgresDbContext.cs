f24a00117a107a211828b8f162ff33ccc638d09f
Damir Mansurov <d.mansurov@mandarin.io>
2024-03-25 10:03:28 +0300

    fixed timestamp

--- ExcalibConsultBot/DAL/ConsultPostgresDbContext.cs
+++ ExcalibConsultBot/DAL/ConsultPostgresDbContext.cs
@@ -3,8 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ExcalibConsultBot.DAL;
 
-public class ConsultPostgresDbContext(DbContextOptions<ConsultPostgresDbContext> options) : DbContext(options)
+public class ConsultPostgresDbContext: DbContext
 {
+    public ConsultPostgresDbContext(DbContextOptions<ConsultPostgresDbContext> options) : base(options)
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    }
     public DbSet<MessageEntity> Messages { get; set; } = null!;
     public DbSet<UserEntity> Users { get; set; } = null!;
     public DbSet<LessonEntity> Lessons { get; set; } = null!;
