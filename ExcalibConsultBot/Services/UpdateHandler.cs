f17bc182d3ee7bf3389774dbfcaa4fb230835443
Damir Mansurov <d.mansurov@mandarin.io>
2024-03-27 09:23:01 +0300

    specify kind

--- ExcalibConsultBot/Services/UpdateHandler.cs
+++ ExcalibConsultBot/Services/UpdateHandler.cs
@@ -123,6 +123,7 @@ public class UpdateHandler : IUpdateHandler
                 var lessonFullText = message.Text.Split(" ");
                 var telegramUserId = long.Parse(lessonFullText[1]);
                 var lessonDate = DateTime.ParseExact(lessonFullText[2], "dd.MM.yyyy/H:mm", CultureInfo.CurrentCulture, DateTimeStyles.None);
+                lessonDate = DateTime.SpecifyKind(lessonDate, DateTimeKind.Local);
                 var userEntity = await _postgresDbContext.Users.FirstAsync(x => x.UserId == telegramUserId, cancellationToken: cancellationToken);
                 var balance = await _postgresDbContext.Balances.FirstAsync(x => x.UserId == userEntity.Id, cancellationToken);
 
